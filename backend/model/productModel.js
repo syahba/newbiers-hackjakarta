253fc8b93ee0f15905b28ae11e1783c98bf131db
W-ptr <wisnu.putra001@binus.ac.id>
2024-07-27 13:45:28 +0700

    feat: create,get product list, and get product by Id

--- backend/model/productModel.js
+++ backend/model/productModel.js
@@ -48,24 +48,34 @@ async function createProduct(newProduct){
     }
 }
 
-async function getAllProduct(newProduct){
+async function getAllProduct(){
     try{
-        await prisma.product.create({
-            data: {
-                id: uuidv4(),
-                name: newProduct.name,
-                type: newProduct.type,
-                price: newProduct.price,
-                image: newProduct.image,
-                description: newProduct.description,
-                score: newProduct.score,
-                grade: newProduct.grade,
-                nutrition: newProduct.nutrition,
-                ingredient: newProduct.ingredient,
-            }
-        });
+        const productList = await prisma.product.findMany();
+        return productList;
+    }
+    catch (err){
+        return err;
+    }
+}
 
-        return true;
+async function getProductById(id){
+    try{
+        const product = await prisma.product.findMany({
+            where: {id:id}
+        });
+        return product;
+    }
+    catch (err){
+        return err;
+    }
+}
+
+async function updateProduct(id){
+    try{
+        const product = await prisma.product.findMany({
+            where: {id:id}
+        });
+        return product;
     }
     catch (err){
         return err;
