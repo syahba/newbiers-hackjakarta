1c1367073d2f688f524073b135cac2f822b30bfa
syahba <ksyaahba@gmail.com>
2024-07-27 13:48:03 +0700

    login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,21 @@
+import '../styles/LoginPage.css';
+
 function LoginPage() {
   return (
-    <div></div>
+    <div className="login-container">
+      <h1 className="font-bold text-[var(--secondary)] text-4xl">Helathy</h1>
+
+      <div className="text-center">
+        <h2 className="text-[var(--neutral)] font-semibold text-xl mb-4">Login</h2>
+
+        <div>
+          <button className="login-btn">Merchant</button>
+          <button className="login-btn">Customer</button>
+        </div>
+      </div>
+
+      <p className="text-[var(--neutral)]">by Newbiers</p>
+    </div>
   );
 }
 
