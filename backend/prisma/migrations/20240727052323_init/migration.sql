dad1a0fd1687964b53afe9faf476414149b423ef
W-ptr <wisnu.putra001@binus.ac.id>
2024-07-27 12:24:26 +0700

    upload backend template

---
+++ backend/prisma/migrations/20240727052323_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "price" BIGINT NOT NULL,
+    "image" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "score" INTEGER NOT NULL,
+    "grade" TEXT NOT NULL,
+    "nutrition" JSONB NOT NULL,
+    "ingredient" JSONB NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "update_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "product_pkey" PRIMARY KEY ("id")
+);
