ee664a07d8480a418118f09ba0fb2a394f8f9324
W-ptr <wisnu.putra001@binus.ac.id>
2024-07-27 15:36:16 +0700

    feat: add upload image api and test upload image: run uploadImageTest.html to trt

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,17 @@
+const apiRouter = require("./controller/apiController");
+const uploadRouter = require("./controller/uploadController");
 const express  = require("express");
-const router = require("./controller/apiController");
+const path = require("path");
 const cors = require("cors");
 require("dotenv").config()
 const app = express();
 
 app.use(cors({origin:"*"}));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/product',router);
+app.use('/api/product',apiRouter);
+app.use('/api/upload',uploadRouter);
 
 app.get('/',(req,res)=>{
     res.send("<h1>Hello</h1>");
---
+++ backend/controller/uploadController.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("node:path");
+const multer = require("multer");
+const {v4:uuidv4} = require("uuid");
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join(__dirname, '..', 'public', 'images'));
+    },
+    filename: (req, file, cb) => {
+        const uniqueName = uuidv4() + '.png'; // Generate a unique name with UUID and set it to PNG
+        cb(null, uniqueName);
+      }
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/', upload.single('image'), (req, res) => {
+    try {
+        const filePath = `/images/${req.file.filename}`;
+        res.json({
+          message: 'Image uploaded successfully',
+          filePath: filePath
+        });
+    } catch (error) {
+        res.status(400).send('Error uploading image');
+    }
+});
+
+module.exports = router;
