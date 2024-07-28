39b4c22f3e053f4133930b9821dd0a6c2fd5806e
Mochamad Aufa <mochamadaufa7@gmail.com>
2024-07-28 09:41:22 +0700

    fix static data

--- backend/helper/const.js
+++ backend/helper/const.js
@@ -3,7 +3,7 @@ const GRADES = {
     grade: "A",
     title: "Nutri-Score A",
     color: "#008439",
-    image: "http://20.172.212.232:8000/images/grade.png",
+    image: "https://get.apicbase.com/nutri-score-science-based-nutritional-value-labelling-system/",
     url: "http://20.172.212.232:8000/images/grade.png",
     description: {
       food: "Foods with a nutrition score from high to 0 This means that foods in this category have the highest nutritional quality.",
@@ -14,7 +14,7 @@ const GRADES = {
     grade: "B",
     title: "Nutri-Score B",
     color: "#75BE00",
-    image: "http://20.172.212.232:8000/images/grade.png",
+    image: "https://get.apicbase.com/nutri-score-science-based-nutritional-value-labelling-system/",
     url: "http://20.172.212.232:8000/images/grade.png",
     description: {
       food: "Foods with a nutrition score from 1 to 2 The nutritional quality is still good, although slightly lower than category A.",
@@ -25,7 +25,7 @@ const GRADES = {
     grade: "C",
     title: "Nutri-Score C",
     color: "#FFC800",
-    image: "http://20.172.212.232:8000/images/grade.png",
+    image: "https://get.apicbase.com/nutri-score-science-based-nutritional-value-labelling-system/",
     url: "http://20.172.212.232:8000/images/grade.png",
     description: {
       food: "Foods with a nutrition score from 3 to 10 The nutritional quality is decent, but not as good as categories A and B.",
@@ -36,7 +36,7 @@ const GRADES = {
     grade: "D",
     title: "Nutri-Score D",
     color: "#FF7800",
-    image: "http://20.172.212.232:8000/images/grade.png",
+    image: "https://get.apicbase.com/nutri-score-science-based-nutritional-value-labelling-system/",
     url: "http://20.172.212.232:8000/images/grade.png",
     description: {
       food: "Foods with a nutrition score from 11 to 18 Their nutritional quality is lower than the previous category.",
@@ -47,7 +47,7 @@ const GRADES = {
     grade: "E",
     title: "Nutri-Score E",
     color: "#FA2300",
-    image: "http://20.172.212.232:8000/images/grade.png",
+    image: "https://get.apicbase.com/nutri-score-science-based-nutritional-value-labelling-system/",
     url: "http://20.172.212.232:8000/images/grade.png",
     description: {
       food: "Food with a nutrition score higher than 18 This is the category with the lowest nutritional quality.",
